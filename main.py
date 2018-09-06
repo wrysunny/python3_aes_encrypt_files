65774abc2df3007ddb44577e0dbb729cd409b01a
wrysunny <liqiuliang2013@gmail.com>
2018-09-06 15:59:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #! -*- coding:utf-8 -*-
 '''
-AES encoding files 
-fu ck u d 
+AES encoding files
+fu ck u d
 '''
 import os
+import sys
 import random
 import string
 import base64
@@ -11,7 +12,7 @@ from Crypto.Cipher import AES
 from binascii import a2b_hex,b2a_hex
 
 
-aes_key = ''.join(random.sample(string.ascii_letters + string.digits,16)).encode('utf-8')
+aes_key = ''.join(random.sample(string.ascii_letters + string.digits,32)).encode('utf-8')
 aes_iv = ''.join(random.sample(string.ascii_letters + string.digits,16)).encode('utf-8')
 print('aes key:',str(aes_key))
 print('aes iv:',str(aes_iv))
@@ -23,7 +24,7 @@ def aeswork(workpath):
 		aes_files = fw.read()
 	aes_files = b2a_hex(aes_files)
 	length = 16 - (len(aes_files) % 16)
-	aes_files += bytes([length])*length 
+	aes_files += bytes([length])*length
 	aes_enfiles = base64.b64encode(cryptor.encrypt(aes_files))
 	with open(workpath + '.bak','wb') as fe:
 		fe.write(aes_enfiles)
@@ -31,22 +32,19 @@ def aeswork(workpath):
 	os.rename(workpath + '.bak', workpath)
 
 
-workdir = '/home/pi/python/test/'
+if len(sys.argv) < 2:
+	print('please input work path')
+	os._exit(0)
+else:
+	workdir = sys.argv[1]
+
+#workdir = '/media/wrysunny/5AD70001D6FFDAE9/TL-WR740N_V8_130417/'
 for dirpath,dirnames,filenames in os.walk(workdir):
 	for f in filenames:
 		workpath = os.path.join(dirpath,f)
 		print('workpath:',workpath)
 		aeswork(workpath)
-		
-#aes加密文件.bak
-#删除原文件 os.remove(file)
-# 加密文件修改为原名称os.rename
-#os.rename(src, dst)
-#参数
-#src -- 要修改的目录名
-#dst -- 修改后的目录名
-# 删除两边空字符 strip()
-# 删除右边空字符 lstrip()
-# 切片处理s[:4]
 
-
+print('work dir :' + workdir)
+with open(workdir + '/aesencrypt','w') as fq:
+	fq.write('aes key:'+str(aes_key.decode('utf8'))+"\n"+'aes iv:'+str(aes_iv.decode('utf8')))
